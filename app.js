46ef653553636893396c16cdbe29b2c3edc4eda5
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-20 11:54:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +13,17 @@ app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.get("/products");
+
+productRouter.route("/").get((req, res) => {
+  res.send("Product page");
+});
+
+productRouter.route("/1").get((req, res) => {
+  res.send("Product page product 1");
+});
+
+app.use("/products", productRouter);
+
 app.get("/", (req, res) => {
   res.render("index", {
     username: "John Doe",
