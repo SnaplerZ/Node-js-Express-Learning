ee9e999ae6342b7effa8d9868fc3676179095ee5
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-20 11:40:30 +0700

    replace index_old.html to index.ejs

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const morgan = require("morgan");
 const path = require("path");
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send(
-    "Hello World! Eula When? I hope 80 roll is enough for me to get her."
-  );
+  res.render("index", {
+    username: "John Doe",
+    customers: ["john doe", "jane doe", "Jonh doe"],
+  });
 });
 
-app.listen(port, () => {
-  debug("Example app listening at http://localhost:%s", chalk.green(port));
+app.listen(PORT, () => {
+  debug("Example app listening at http://localhost:%s", chalk.green(PORT));
 });
