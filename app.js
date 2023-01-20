c0ea05d9e566bd60b4fc22ce8c0fb80740d8616a
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-20 10:08:40 +0700

    Add all file to git
    Hello World :100:

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3001;
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! test123");
+});
+
+app.listen(port, () => {
+  debug("Example app listening at http://localhost:%s", chalk.green(port));
+});
