f79cd6a29c907e3d975b7d2b293a97fb249c825e
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-27 16:02:29 +0700

    json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const products = require("./data/products.json");
 const productRouter = express.Router();
 
 const app = express();
@@ -16,28 +17,7 @@ app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
   res.render("products", {
-    products: [
-      {
-        productName: "product 1",
-        productPrice: 100,
-        productDescription: "product 1 description",
-      },
-      {
-        productName: "product 2",
-        productPrice: 200,
-        productDescription: "product 2 description",
-      },
-      {
-        productName: "product 3",
-        productPrice: 300,
-        productDescription: "product 3 description",
-      },
-      {
-        productName: "product 4",
-        productPrice: 400,
-        productDescription: "product 4 description",
-      },
-    ],
+    products,
   });
 });
 
