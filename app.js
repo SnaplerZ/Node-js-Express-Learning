ece47283ae07726938c72f6bd153119d10599c29
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-27 16:11:06 +0700

    js

--- app.js
+++ app.js
@@ -16,30 +16,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-  res.render("products", {
-    products: [
-      {
-        productName: "product 1",
-        productPrice: 100,
-        productDescription: "product 1 description",
-      },
-      {
-        productName: "product 2",
-        productPrice: 200,
-        productDescription: "product 2 description",
-      },
-      {
-        productName: "product 3",
-        productPrice: 300,
-        productDescription: "product 3 description",
-      },
-      {
-        productName: "product 4",
-        productPrice: 400,
-        productDescription: "product 4 description",
-      },
-    ],
-  });
+  res.render("products", products);
 });
 
 productRouter.route("/1").get((req, res) => {
