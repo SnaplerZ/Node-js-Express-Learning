83061af37c2b8962e9b5524b2d0232dd79cc8f70
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-30 11:29:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-  res.render("products", products);
+  res.render("products", { products });
 });
 
 productRouter.route("/:id").get((req, res) => {
