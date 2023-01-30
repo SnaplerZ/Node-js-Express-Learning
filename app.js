b967f6614485f5dc92b89f2bd8963767214cd217
SnaplerZ <119152603+SnaplerZ@users.noreply.github.com>
2023-01-30 11:40:47 +0700

    router folder

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const products = require("./data/products.json");
-const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -15,19 +14,7 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-  res.render("products", { products });
-});
-
-productRouter.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  res.render("product", {
-    product: products[id],
-  });
-  // res.send("Product page product" + id);
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {
